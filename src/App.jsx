cc4979977e4276b0d8b12a01b103fc8ecb02c638
Juan Valencia <valenciacamilo5926@gmail.com>
2025-01-29 19:19:46 -0500

    Se solucionó un problema que tenia la carpeta src como un submodulo

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const urlBase = "https://api.openweathermap.org/data/2.5/weather";
+  const claveApi = "c43fc3b8b9a79416c7c175b3d1bf3478";
+  const convertirACelsius = 273.15;
+
+  const [ciudad, setCiudad] = useState("");
+  const [datosClima, setDatosClima] = useState(null);
+
+  const handleCambioCiudad = (e) => {
+    setCiudad(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (ciudad.length > 0) fetchClima();
+  };
+
+  const fetchClima = async () => {
+    try {
+      const response = await fetch(`${urlBase}?q=${ciudad}&appid=${claveApi}`);
+      const datos = await response.json();
+      setDatosClima(datos);
+    } catch (error) {
+      console.error("Ocurrió el siguiente problema: ", error);
+    }
+  };
+
+  return (
+    <div className="contenedor">
+      <h1>App del Clima</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={ciudad} onChange={handleCambioCiudad} />
+        <button type="submit">Buscar</button>
+      </form>
+      {datosClima && (
+        <div id="clima">
+          <h2>{datosClima.name}</h2>
+          <img
+            src={`https://openweathermap.org/img/wn/${datosClima.weather[0].icon}@2x.png`}
+          />
+          <p>
+            Temperatura: 
+            <span>
+              {parseInt(datosClima?.main?.temp - convertirACelsius)}ºC
+            </span>
+          </p>
+          <p>
+            Condición meteorológica:
+            <span>{datosClima.weather[0].description}</span>
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
